584ba6b92074a949d37c6268417d4e4c38856b6e
Andrew Koster <andrewkoster95@gmail.com>
2021-08-17 21:31:55 -0400

    fixing errors

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,14 +2,14 @@ const CACHE_NAME = 'static-cache-v2';
 const DATA_CACHE_NAME = 'data-cache-v1';
 const FILES_TO_CACHE = [
     '/',
-    "/manifest.webmanifest",
-    "/styles.css",
+    '/manifest.webmanifest',
+    '/styles.css',
     '/db.js',
-    '/icons/icon-192x192.png',
-    '/icons/icon-512x512.png',
     '/index.html',
     '/index.js',
-]
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png'
+];
 
 self.addEventListener('install', function (evt) {
     evt.waitUntil(
